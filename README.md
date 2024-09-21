9792b42a01e193e6536b5612eebdfcbe9832e9b0
0000xFFFF <coldmailgg@gmail.com>
2024-09-21 03:34:14 +0200

    added install ; uninstall scripts ; readme ; preview image

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# gridview
+# gridview - grid image viewer
+
+[![Python 3.12.5](https://img.shields.io/badge/Python-3.12.5-yellow.svg)](http://www.python.org/download/)
+
+<img src="preview/preview.png" width="970" height="835">
+
+# Installation
+```
+./setup-env.sh
+./install.sh
+```
+
+## Requirements
+* flask
+* pyopenssl
+* flask_sslify
+
+# Running
+```
+gridview        # inside the dir you want to view media files
+```
---
+++ server.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+from flask import Flask, request, send_from_directory, jsonify
+import os
+import argparse
+
+dir_script = os.path.dirname(os.path.realpath(__file__))
+dir_templates = os.path.join(dir_script, "templates")
+dir_static = os.path.join(dir_script, "static")
+
+app = Flask(
+    __name__,
+    static_url_path='',
+    static_folder=dir_static
+    )
+
+# Folder to serve media files from
+MEDIA_FOLDER = os.getcwd()
+
+# Helper function to get all media files recursively
+def get_media_files(directory):
+    media_files = []
+    for root, dirs, files in os.walk(directory):
+        relative_path = os.path.relpath(root, MEDIA_FOLDER)
+        media_files.append({
+            'path': relative_path,
+            'files': [f for f in files if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.mp4', '.mov', '.avi'))]
+        })
+    return media_files
+
+
+
+@app.route('/')
+def index():
+    return send_from_directory(dir_templates, 'index.html')
+
+# Route to serve media files
+@app.route('/media/<path:filepath>')
+def media(filepath):
+    return send_from_directory(MEDIA_FOLDER, filepath)
+
+# Route to get list of all media files recursively
+@app.route('/media-list')
+def media_list():
+    files = get_media_files(MEDIA_FOLDER)
+    return jsonify(files)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
