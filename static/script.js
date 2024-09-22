e52811c5c41ec7698ce03cfc963c321ec8606e79
0000xFFFF <coldmailgg@gmail.com>
2024-09-22 18:22:14 +0200

    hoverzoom img popup -- also added checkbox settings

--- static/script.js
+++ static/script.js
@@ -1,4 +1,30 @@
-// Fetch the list of media files from the server
+// settings here
+let setting_hoverZoom = false;
+
+function topright_controls() {
+    // top right container
+	let div_trcont = document.createElement("div");
+	div_trcont.id = "trcont";
+	div_trcont.style = "position: fixed; top: 5px; right: 5px;";
+
+	let label = document.createElement("label");
+	label.className = "checkboxContainer";
+	label.title = "enable zoom on hover";
+	label.id = "trcheckbox";
+	let input = document.createElement("input");
+	input.type = "checkbox";
+	input.checked = setting_hoverZoom;
+	input.addEventListener("change", function() { setting_hoverZoom = !setting_hoverZoom; });
+	let span = document.createElement("span");
+	span.className = "checkmark";
+	label.appendChild(input);
+	label.appendChild(span);
+    div_trcont.append(label);
+    document.body.appendChild(div_trcont);
+}
+
+topright_controls();
+
 fetch('/media-list')
     .then(response => response.json())
     .then(directories => {
@@ -21,19 +47,19 @@ fetch('/media-list')
             div_dir_files.className = 'media-dir-files';
 
             dir.files.forEach(file => {
-                let name = file['name'];
+                const name = file['name'];
                 const div_file = document.createElement('div');
                 div_file.className = 'media-file';
 
                 // info
-                let div_file_info = document.createElement('p');
+                const div_file_info = document.createElement('p');
                 div_file_info.className = 'media-file-info';
                 div_file_info.textContent = `${file.width}x${file.height}  ${file.name}`;
-                div_file.addEventListener('mouseover', function(event) {
+                div_file.addEventListener('mouseover', function() {
                     div_file_info.style.display = 'block';
                     div_file_info.style.opacity = '1'; // Make it visible
                 });
-                div_file.addEventListener('mouseleave', function(event) {
+                div_file.addEventListener('mouseleave', function() {
                     div_file_info.style.opacity = '0'; // Hide with transition
                     setTimeout(() => {
                         div_file_info.style.display = 'none'; // Hide after transition
@@ -50,12 +76,29 @@ fetch('/media-list')
                         //img.src = "/path/to/placeholder/image.png"; // maybe add placeholder
                     };
                     div_file.appendChild(img);
+
+                    // image popup on hover
+                    const img_popup = document.createElement('img');
+                    img_popup.className = 'media-file-popup';
+                    document.body.appendChild(img_popup);
+                    div_file.addEventListener('mouseenter', function () {
+                        if (!setting_hoverZoom) { return; }
+                        img_popup.src = img.src;
+                        img_popup.style.display = 'block';
+                    });
+                    div_file.addEventListener('mouseleave', function () {
+                        if (!setting_hoverZoom) { return; }
+                        img_popup.src = '';
+                        img_popup.style.display = 'none';
+                    })
                 }
                 else if (name.endsWith('.mp4') || name.endsWith('.webm') || name.endsWith('.mov') || name.endsWith('.avi')) {
                     const video = document.createElement('video');
                     video.src = `/media/${dir_path}${name}`;
                     video.controls = true;
                     div_file.appendChild(video);
+                    
+                    // TODO: make thumbnail instead -- when clicked turn it into video
                 }
                 else {
                     // todo: handle other file types maybe?
@@ -68,28 +111,3 @@ fetch('/media-list')
         });
     })
     .catch(error => console.error('Error fetching media list:', error));
-
-
-// // might need in the future idk
-// document.addEventListener("DOMContentLoaded", function () {
-//     const mediaDirFiles = document.querySelector('.media-dir-files');
-//     const mediaFiles = document.querySelectorAll('.media-file');
-//     function adjustMasonry() {
-//         let columnHeights = [];
-//         mediaFiles.forEach(file => {
-//             const media = file.querySelector('img, video');
-//             if (media.complete) {
-//                 const height = media.clientHeight;
-//                 file.style.gridRowEnd = `span ${Math.ceil(height / 10)}`;
-//             } else {
-//                 media.onload = () => {
-//                     const height = media.clientHeight;
-//                     file.style.gridRowEnd = `span ${Math.ceil(height / 10)}`;
-//                 };
-//             }
-//         });
-//     }
-//     // Adjust on load and on window resize
-//     window.addEventListener('load', adjustMasonry);
-//     window.addEventListener('resize', adjustMasonry);
-// });
